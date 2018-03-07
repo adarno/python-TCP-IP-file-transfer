32c56c0ce8920024a744e37f628e5d080322735a
Adrian Arnold <adrian_arnold@yahoo.de>
2018-03-07 16:14:30 +0100

    first attempt of multithreaded

--- server.py
+++ server.py
@@ -3,50 +3,66 @@ import sys
 import threading
 
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class ServerThread(threading.Thread):
 
-# Bind the socket to the port
-server_address = ('localhost', 8070)
-print >> sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+    def __init__(self, client_ip, client_port, client_socket):
+        threading.Thread.__init__(self)
+        self.client_ip = client_ip
+        self.client_port = client_port
+        self.client_socket = client_socket
+        print "[+] New thread started for "+ip+":"+str(port)
 
-# Listen for incoming connections
-sock.listen(5)
+    def run(self):
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-
-    text = ""
-
-    try:
-        print >> sys.stderr, 'connection from', client_address
-
-        # Receive the data in small chunks
         while True:
-            data = connection.recv(1024)
-            print("received data ... ")
 
-            if data:
-                text += data
-                response = "transfer successfull"
-                try:
-                    connection.send(response)
-                except:
-                    pass
-            else:
-                print(client_address + " closed the connection.")
-                break
+            text = ""
+
+            try:
+                print >> sys.stderr, 'connection from', self.client_ip, ":", self.client_port
+
+                # Receive the data in small chunks
+                while True:
+                    data = self.client_socket.recv(1024)
+                    print("received data ... ")
+
+                    if data:
+                        text += data
+                        response = "transfer successfull"
+                        try:
+                            self.client_socket.send(response)
+                        except:
+                            pass
+                    else:
+                        print(self.client_socket + " closed the connection.")
+                        break
 
 
-    except:
-        # Clean up the connection
-        connection.close()
+            except:
+                # Clean up the connection
+                self.client_socket.close()
 
-    # write data to file
-    file_obj = open("recv.sat", "w")
-    print("writing data to file")
-    file_obj.write(text)
-    file_obj.close()
+            # write data to file
+            file_obj = open("recv.sat", "w")
+            print("writing data to file")
+            file_obj.write(text)
+            file_obj.close()
+
+
+if __name__ == "__main__":
+
+    host = "127.0.0.1"
+    port = 8070
+
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    tcpsock.bind((host,port))
+    threads = []
+
+    tcpsock.listen(4)
+    print "\nListening for incoming connections..."
+    (clientsock, (ip, port)) = tcpsock.accept()
+    newthread = ServerThread(ip, port, clientsock)
+    newthread.start()
+    threads.append(newthread)
